500480d4d56ec15e1ebc12a1c13c81b27b4d1db2
vladislaveeeeee <roshkovlad2007@gmail.com>
2023-09-28 23:03:46 +0300

    Add project files.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static string IsPalindrome(string palindrom)
+        {
+            char[] charArray = palindrom.ToCharArray();
+            Array.Reverse(charArray);
+            string reversed = new string(charArray);
+
+            if (palindrom == reversed)
+            {
+                return "Yes";
+            }
+            else
+            {
+                return "No";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string palindrom = Console.ReadLine();
+
+            Console.WriteLine(palindrom);
+            Console.WriteLine(IsPalindrome(palindrom));
+        }
+    }
+}
